927f8d172a5965ec0fca48e6294d644d5dd0fdcb
André Stephano <andrestephano@gmail.com>
2016-06-24 22:45:03 -0300

    beautify

--- README.md
+++ README.md
@@ -32,6 +32,30 @@ end
 # goomba!
 ```
 
+```ruby
+require 'date'
+
+def add_day(date, days = 1)
+  date + days
+end
+
+result_date = pipe Date.today do
+           add_day
+           add_day 8
+         end
+
+result_date.should > Date.today + 8
+result_date.should < Date.today + 10
+```
+
+Working if external dependencies
+```ruby
+pipe 0.0 do
+  e -> (entry_value, &b) { Math.cos entry_value }
+end
+# => 1.0
+```
+
 ## License
 
 The gem is available as open source under the terms of the [MIT License](http://opensource.org/licenses/MIT).
--- spec/pipeblock_spec.rb
+++ spec/pipeblock_spec.rb
@@ -5,6 +5,7 @@ describe Pipeblock do
     result = pipe 1 do
                 inspect
               end
+
     result.should == '1'
   end
 
@@ -13,9 +14,11 @@ describe Pipeblock do
     def sum(a, b)
       a + b
     end
+
     result = pipe 1 do
                sum(2)
              end
+
     result.should == 3
   end
 
@@ -23,10 +26,12 @@ describe Pipeblock do
     def concatenate(a, b)
       a + b
     end
+
     result = pipe 'Hey' do
-      concatenate(' you!')
-      concatenate(' Can you hear me?')
-    end
+               concatenate(' you!')
+               concatenate(' Can you hear me?')
+             end
+
     result.should == ('Hey you! Can you hear me?')
   end
 
@@ -34,16 +39,19 @@ describe Pipeblock do
     def sin(number)
       Math.sin(number)
     end
+
     result = pipe 0.0 do
-      sin
-    end
+               sin
+             end
+
     result.should == 0.0
   end
 
-  it 'should work with external encapsuled on lambda' do
+  it 'should work with external method if it is encapsuled on lambda' do
     result = pipe 0.0 do
-      e -> (entry_value, &b) { Math.cos entry_value }
-    end
+               e -> (entry_value, &b) { Math.cos entry_value }
+             end
+
     result.should == 1.0
   end
 
